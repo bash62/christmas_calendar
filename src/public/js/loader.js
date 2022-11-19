a63ded08409f32a238fad8a5776b633065dda148
Hokanosekai <arsouille.raillard@gmail.com>
2022-11-19 22:38:59 +0100

    Fix fullscreen photo caption

--- src/public/js/loader.js
+++ src/public/js/loader.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
         const containers = document.querySelectorAll(['header', 'main', 'footer']);
         const loader_container = document.getElementById('loader-container');
+        const snowContainer = document.getElementById('snow-container');
 
         containers.forEach(container => {
             container.classList.remove('hidden');
@@ -10,6 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         loader_container.classList.add('hidden');
         loader_container.classList.add('animate-fade-out-loader');
-
+        snowContainer.classList.remove('hidden');
     }, Math.random() * 1000 + 2500);
 });
